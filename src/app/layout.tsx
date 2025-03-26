e81c34502cfc9596513a8619bc60b5a5d21c7ce1
MPSxDev <mparedes.digital@gmail.com>
2025-03-26 20:45:58 -0300

    feat: change favicon to tradoxus

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,27 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Tradoxus - Coming Soon",
-  description: "Tradoxus - We're working on something amazing. Stay tuned!",
+  title: "Tradoxus",
+  description: "Coming Soon - We're working on something amazing!",
+  icons: {
+    icon: "/logo_isotipe.svg",
+    shortcut: "/logo_isotipe.svg",
+    apple: "/logo_isotipe.svg",
+  },
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
